de558ecb8b0ab32c27f1c655bfd6e2826ca6bd1c
opapzayo <apsiwagorn@hotmail.com>
2021-08-24 12:02:05 +0700

    add file upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import 'cypress-file-upload';
+import './commands';
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
